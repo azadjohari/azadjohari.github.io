ba1cda6012d708076cbc156b8e80ac195b6d1945
Azad JOHARI <azad.johari@bureauveritas.com>
2020-10-23 21:40:17 +0800

    remove default row

--- table-booking-app/scripts/list.js
+++ table-booking-app/scripts/list.js
@@ -25,7 +25,7 @@ function GetBookings() {
                 row.insertCell(2).innerHTML = gEmail;
                 row.insertCell(3).innerHTML = gPax;
                 row.insertCell(4).innerHTML = ""; //Remarks
-                row.insertCell(5).innerHTML = "<button id='" + buttonId + "'>Delete</button><br/>";
+                row.insertCell(5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button><br/>";
 
                 bookingIds.push(buttonId);
 
