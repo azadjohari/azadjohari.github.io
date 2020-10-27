05bd30cc8e0b0e3e9f4858e5373408e8e134c27b
Azad JOHARI <azad.johari@bureauveritas.com>
2020-10-27 15:43:28 +0800

    updated the linkURL

--- vuejs/scripts/index.js
+++ vuejs/scripts/index.js
@@ -9,7 +9,7 @@ var app2 = new Vue({
     el: '#app-2',
     data: {
         message: 'You loaded this page on ' + new Date().toLocaleString(),
-        linkUrl: "https://wwww.metallica.com",
+        linkUrl: "https://www.metallica.com",
         linkText: "Metallica"
     }
 })
