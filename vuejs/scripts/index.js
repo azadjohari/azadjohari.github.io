b6c2a75d442e6c084087ccf163671520aaf03161
Azad JOHARI <azad.johari@bureauveritas.com>
2020-10-30 20:12:40 +0800

    added linkURL & linkText

--- vuejs/scripts/index.js
+++ vuejs/scripts/index.js
@@ -8,6 +8,8 @@ var app = new Vue({
 var app2 = new Vue({
     el: "#app-2",
     data: {
-        message: "You loaded this page on " + new Date().toLocaleString()
+        message: "You loaded this page on " + new Date().toLocaleString(),
+        linkURL: "https://www.google.com.my",
+        linkText: "Go to Google Malaysia!"
     }
 });
